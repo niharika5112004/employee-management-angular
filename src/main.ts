ceb433fe73d0c9dc3ac93f3ca6771ed447062a81
niharika5112004 <niharika.dusane@gmail.com>
2026-04-13 16:35:34 +0530

    dashbaord fixes

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { App } from './app/app';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 
-// ✅ ADD THESE
+
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './app/interceptors/auth.interceptor';
 
@@ -11,7 +11,7 @@ bootstrapApplication(App, {
   providers: [
     provideRouter(routes),
 
-    // ✅ ADD THIS LINE
+    
     provideHttpClient(withInterceptors([authInterceptor]))
     
   ]
